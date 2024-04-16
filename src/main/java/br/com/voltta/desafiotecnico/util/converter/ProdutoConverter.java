7cf9dc05b322f01f1a3a79ad482443b4c0c799ff
erickribeirovoltta <166430795+erickribeirovoltta@users.noreply.github.com>
2024-04-15 22:20:32 -0300

    fix produto converter

--- src/main/java/br/com/voltta/desafiotecnico/util/converter/ProdutoConverter.java
+++ src/main/java/br/com/voltta/desafiotecnico/util/converter/ProdutoConverter.java
@@ -1,6 +1,7 @@
 package br.com.voltta.desafiotecnico.util.converter;
 
 import br.com.voltta.desafiotecnico.dto.ProdutoDto;
+import br.com.voltta.desafiotecnico.dto.ProdutoInsertDto;
 import br.com.voltta.desafiotecnico.entity.ProdutoEntity;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,10 @@ public class ProdutoConverter {
     public ProdutoDto mapToDto(ProdutoEntity entity){
         ProdutoDto dto = new ProdutoDto(entity);
         return dto;
+    }
+
+    public ProdutoInsertDto mapToInsertDto(ProdutoEntity entity){
+        return new ProdutoInsertDto(entity);
     }
 
     public ProdutoEntity mapToEntity(ProdutoDto dto){
